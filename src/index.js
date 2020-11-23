3181c3ca34c576990ef640328506a2a9d50ef959
dr-dolce14 <dolcemascop1@gmail.com>
2020-11-23 10:52:12 -0500

    multiple comments in app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     
@@ -7,7 +8,7 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"/>
+                    <img alt="avatar" src={faker.image.people()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
@@ -22,7 +23,40 @@ const App = () => {
                     </div>
                 </div>
             </div>
-            
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.people()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6pm</span>
+
+                    </div>
+                    <div className="text">
+                        Nice Blog Post!
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.people()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6pm</span>
+
+                    </div>
+                    <div className="text">
+                        Nice Blog Post!
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
