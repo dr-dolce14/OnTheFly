3f42f4c7757396fb4b300f41cc29a8e4f7e9d763
dr-dolce14 <dolcemascop1@gmail.com>
2020-11-25 13:53:39 -0500

    refactored app to class component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err) 
-    );
 
-    return (
-    <div>
-        Hi there!
-       
-    </div>
-    )
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err) 
+        );
+        return <div>Latitude: </div>;
+    }
 }
 
 
