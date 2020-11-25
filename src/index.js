892da781ba6680ad14b651ce06fa13b7d50d48a6
dr-dolce14 <dolcemascop1@gmail.com>
2020-11-25 13:23:42 -0500

    deleted comments

--- src/index.js
+++ src/index.js
@@ -1,40 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
-    
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45pm" 
-                commentText="Holy cow!" 
-                avatar={faker.image.image()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 2:00am" 
-                commentText="I totally agree!" 
-                avatar={faker.image.image()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author="Lina" 
-                timeAgo="Yesterday at 10:15pm" 
-                commentText="That's not cool" 
-                avatar={faker.image.image()}
-                />
-            </ApprovalCard>
-        </div>
-    );
-};
+    return <div>Hi there!</div>
+}
+
+
 
 ReactDOM.render(
     <App />, 
