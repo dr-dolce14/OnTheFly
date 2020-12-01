0861d4f7f5c7523e86290121ed03109ba3645e7f
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-01 15:36:44 -0500

    added constructor to app class

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+
 
 
 
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({ lat: position.coords.latitude})
+            },
             (err) => console.log(err) 
         );
-        return <div>Latitude: </div>;
+    }
+
+    render() {
+      
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
+
+
 
 
 
