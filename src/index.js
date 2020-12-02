95ff94b85a2b16138cfccd1bcda786cd823d8e2d
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-02 10:52:56 -0500

    refactored componentdidmount and state

--- src/index.js
+++ src/index.js
@@ -7,28 +7,18 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: ''};
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude})
-            },
-            (err) => {
+            (position) => 
+                this.setState({ lat: position.coords.latitude}),
+            (err) => 
                 this.setState({ errorMessage: err.message })
-            }
         );
     }
 
-    componentDidMount() {
-        console.log("My component was rendered to the screen");
-    }
-
-    componentDidUpdate() {
-        console.log("My component was just updated - it rerendered!")
-    }
+ 
 
     render() {
         
