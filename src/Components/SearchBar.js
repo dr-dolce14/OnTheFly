b4013d46d92b25c09bd38c5622c8f5d639c71b85
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-16 12:33:16 -0500

    can uppercase searchbar input with controlled form

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                     <label>Image Search</label>
-                    <input type="text" value={this.state.term} onChange={(event) => this.setState({ term: event.target.value})}/>
+                    <input type="text" value={this.state.term} onChange={(event) => this.setState({ term: event.target.value.toUpperCase()})}/>
                     </div>
                 </form>
             </div>
