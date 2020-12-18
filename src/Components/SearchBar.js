5a42976db67e8fe26b69d9f7a11af01b89541232
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-18 10:45:22 -0500

    how to make sure this is bound

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,14 +6,30 @@ class SearchBar extends React.Component {
 
     state = { term: ''}
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term)
+    }
+
+    // onFormSubmit(event) { 
+        // e.preventDefault();
+        //console.log(this.state.term) <-  THROWS AN 'UNDEFINED' ERROR! BECAUSE YOU NEED AN ARROW FUNCTION FOR 'THIS' TO WORK PROPERLY
+    //  arrow functions AUTOMATICALLY bind 'this'
+    //}
+
+    // could also do <form onSubmit={(event) => onFormSubmit(event)}
+
 
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                     <label>Image Search</label>
-                    <input type="text" value={this.state.term} onChange={(event) => this.setState({ term: event.target.value.toUpperCase()})}/>
+                    <input 
+                    type="text" 
+                    value={this.state.term} 
+                    onChange={(event) => this.setState({ term: event.target.value})}/>
                     </div>
                 </form>
             </div>
