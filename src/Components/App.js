3d792b70bb2e2ffb0b5f380922dac14c718a2b15
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-26 11:38:58 -0500

    filling out axios function

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
 
   onSearchSubmit = (term) => {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {    //the params will add on to this request, as if you were typing /photos?query='fish'
+        params: { query: term },
+        headers: {
+            Authorization: 'Client-ID wHtvmQkhhfxmjd2P6Wt6zXuTpW7DOj7dtfR-PNbHSE0'
+        }
+    } )
+    // this function takes two separate arguments:
+    // 1. the address to which we want to make a GET request
+    // 2. an object which will contain a number of options that will customize our request
   }
 
 
