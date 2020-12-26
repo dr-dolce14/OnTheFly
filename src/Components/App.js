2d9dea8f0c1d25d32e227099003a9be28da329ac
dr-dolce14 <dolcemascop1@gmail.com>
2020-12-26 12:03:32 -0500

    using async keyword

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,16 +5,14 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit = (term) => {
-    axios.get('https://api.unsplash.com/search/photos', {    //the params will add on to this request, as if you were typing /photos?query='fish'
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {    
         params: { query: term },
         headers: {
             Authorization: 'Client-ID wHtvmQkhhfxmjd2P6Wt6zXuTpW7DOj7dtfR-PNbHSE0'
         }
-    } )
-    // this function takes two separate arguments:
-    // 1. the address to which we want to make a GET request
-    // 2. an object which will contain a number of options that will customize our request
+    });
+    console.log(response.data.results)
   }
 
 
